c22d7c89a34559c2a1d8cd50a3adfc2eaf798107
Udit <guru.udit@gmail.com>
2018-01-16 12:42:53 +0530

    second of all

--- suboptions.js
+++ suboptions.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { View,Text, Button,TouchableOpacity ,TextInput, AsyncStorage, FlatList, StyleSheet, ScrollView,ActivityIndicator, ListView} from 'react-native';
+import { View,Text, Button,TouchableOpacity ,TextInput, AsyncStorage, FlatList, StyleSheet, ScrollView,ActivityIndicator, ListView, Image, Dimensions} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import { List, ListItem, Rating } from 'react-native-elements';
 import GridView from 'react-native-super-grid';
 import Home from './App';
+import Carousel from 'react-native-snap-carousel';
+const sliderWidth = Dimensions.get('window');
 
 
 class subOptions extends React.Component {
@@ -35,36 +37,63 @@ static navigationOptions = {
         });
 
     }
-
+    _renderItem ({item, index}) {
+        return (
+            <View style={styles.itemContainer}>
+                <Image style={styles.imageSlide} source={require('./Bajaj-Allianz.jpg')} />
+                <View style={styles.text} >
+                <Text style={styles.title}>{ item.name }</Text>
+                <Text style={styles.title}>{ item.code }</Text>
+              </View>
+            </View>
+        );
+    }
     render() {
+      const items = [
+        { name: 'General Service', code: '#1abc9c' }, { name: 'Repair', code: '#2ecc71' },
+        { name: 'Car Wash', code: '#3498db' }, { name: 'Painting', code: '#9b59b6' },
+        { name: 'Road Support', code: '#16a085' },{ name: 'Offers', code: '#27ae60' }
+
+      ];
       if (this.state.isLoading) {
         return (
-          <View style={{flex: 1, paddingTop: 20}}>
+          <View style={{flex: 1, padding: 50}}>
             <ActivityIndicator />
           </View>
         );
       }
 
       return (
-        <GridView
+        <View style={styles.container}>
+          <ScrollView>
+          <Carousel
 
-         itemDimension={100}
-         items={this.state.dataSource}
-         style={styles.gridView}
-         renderItem={item => (
-           <TouchableOpacity  onPress={() => this.props.navigation.navigate("Three")} style={styles.itemContainer}>
-             <Text style={styles.itemName}>{item.id}</Text>
-             <Text style={styles.itemCode}>{item.email}</Text>
-           </TouchableOpacity>
-         )}
-       />
+                  style={styles.slide}
+                  ref={(c) => { this._carousel = c; }}
+                  data={items}
+                  renderItem={this._renderItem}
+                  sliderWidth={sliderWidth.width}
+                  itemWidth={250}
+                  containerCustomStyle={{ flex: 1 }}
+                  slideStyle={{ flex: 1 }}
+                />
+                </ScrollView>
+        </View>
       );
     }
   }
   const styles = StyleSheet.create({
       container: {
        flex: 1,
-       paddingTop: 22
+       paddingTop: 22,
+      },
+      slide: {
+        flex: 1,
+        backgroundColor: "grey"
+      },
+      text:{
+        backgroundColor: "#00000050",
+
       },
       sectionHeader: {
         paddingTop: 2,
@@ -74,6 +103,19 @@ static navigationOptions = {
         fontSize: 14,
         fontWeight: 'bold',
         backgroundColor: 'rgba(247,247,247,1.0)',
+      },
+      imageSlide:{
+        height : 300,
+        width: 250,
+        position :'absolute',
+        borderRadius : 15
+      },
+      title: {
+        color: "white",
+        margin : 15,
+        overflow:'hidden',
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15
       },
       item: {
         padding: 10,
@@ -87,10 +129,11 @@ static navigationOptions = {
       },
       itemContainer: {
         justifyContent: 'flex-end',
-        borderRadius: 5,
-        padding: 10,
-        height: 100,
-        backgroundColor : "#064"
+        borderRadius: 15,
+        marginTop: 10,
+        height: 300,
+        width : 250,
+        backgroundColor : "#064",
       },
       itemName: {
         fontSize: 16,
